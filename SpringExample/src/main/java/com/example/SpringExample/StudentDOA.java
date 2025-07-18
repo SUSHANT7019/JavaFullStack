02487d7da417f13f3774248f364caa7e604d0cc7
SUSHANT7019 <thadgesushant2007@gmail.com>
2025-07-18 20:11:34 +0530

    Add files via upload

---
+++ SpringExample/src/main/java/com/example/SpringExample/StudentDOA.java
@@ -0,0 +1,37 @@
+package com.example.SpringExample;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentDOA {
+@Autowired
+private final StudRepo studRepo;
+public StudentDOA(StudRepo studRepo) {
+	this.studRepo = studRepo;
+}
+// Fetch student by ID
+public  StudentEntity getStudentById(Long id) {
+	return studRepo.findById(id).orElse(null);
+}
+// Fetch all students
+public List<StudentEntity> getAllStudents() {
+	return studRepo.findAll();
+}
+// Add new student
+public void addStudent(StudentEntity student) {
+	studRepo.save(student);
+}
+// Update student
+public void updateStudent(StudentEntity student) {
+	studRepo.save(student);
+}
+// Delete student by ID
+public void deleteStudent(Long id) {
+	studRepo.deleteById(id);
+}
+
+
+}
